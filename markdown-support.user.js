be9373e2aed48c72416c83b99343d9e7a1993ae1
devTeaa <devtea@protonmail.com>
2019-01-24 13:22:51 +0700

    removed headers markdown

--- markdown-support.user.js
+++ markdown-support.user.js
@@ -14,37 +14,10 @@
 
   let boldReg = `(\\*\\*|__)`;
   let italicReg = `(\\*|_)`;
-  let h1Reg = `(#)`;
-  let h2Reg = `(##)`;
-  let h3Reg = `(###)`;
 
   elementToWatch.forEach(element => {
     document.querySelectorAll(element).forEach(el => {
       let detectedWords = [];
-
-      // Headings 3;
-      detectedWords = el.innerHTML.match(new RegExp(`${h3Reg}.+(<br>)`, "g"));
-      if (detectedWords) {
-        detectedWords.forEach(x => {
-          el.innerHTML = el.innerHTML.replace(x, `<h3>${x.replace(new RegExp(`${h3Reg}`, "g"), "").replace(new RegExp(`(<br>)`), "</h3><br>")}`);
-        });
-      }
-
-      // Headings 2;
-      detectedWords = el.innerHTML.match(new RegExp(`${h2Reg}.+(<br>)`, "g"));
-      if (detectedWords) {
-        detectedWords.forEach(x => {
-          el.innerHTML = el.innerHTML.replace(x, `<h2>${x.replace(new RegExp(`${h2Reg}`, "g"), "").replace(new RegExp(`(<br>)`), "</h2><br>")}`);
-        });
-      }
-
-      // Headings 1;
-      detectedWords = el.innerHTML.match(new RegExp(`${h1Reg}.+(<br>)`, "g"));
-      if (detectedWords) {
-        detectedWords.forEach(x => {
-          el.innerHTML = el.innerHTML.replace(x, `<h1>${x.replace(new RegExp(`${h1Reg}`, "g"), "").replace(new RegExp(`(<br>)`), "</h1><br>")}`);
-        });
-      }
 
       // Bold words
       detectedWords = el.innerHTML.match(new RegExp(`${boldReg}.+${boldReg}`, "g"));
